2333315be7df2f3aae766cc44d5f32dbef0b84d7
shingo.miyazawa <miyazawa@degica.com>
2013-05-20 17:13:40 +0900

    modify for japanese user. change japanase and name input order of for for japan.

--- app/overrides/kana_address_fields.rb
+++ app/overrides/kana_address_fields.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path: "spree/address/_form",
   name: "kana_address_fields",
-  insert_top: '.inner',
+  insert_after: '.inner p:nth-child(2)',
   partial: "spree/address/kana",
   disabled: false
 )
